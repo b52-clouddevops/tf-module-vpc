67fececd1e5ba70170b86aae55b344e6b60ada6f
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:12:20 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "public-subnet" {
   cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "robot-public-subnet-${var.coun}"
+    Name = "robot-public-subnet-${elemt}"
   }
 }
 
