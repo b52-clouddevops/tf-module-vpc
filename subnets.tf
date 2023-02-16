4431097b4ecdf7d8f667832c265eda1b2069e412
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:17:21 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -2,8 +2,8 @@
 resource "aws_subnet" "public-subnet" {
   
   count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  vpc_id           = aws_vpc.main.id
+  cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index)
   availability_zone
 
   tags = {
