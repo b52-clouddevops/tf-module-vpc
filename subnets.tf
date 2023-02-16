c3950d4cef0d436bada5ccf0dccdde6eabbee2b8
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:10:25 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "main" {
   
   count      = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.)
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
     Name = "Main"
