6951bd7e59f5310a54e8ef316a4c8fc944cbc4fb
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:19:52 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,5 @@
 # Creates public subnets based on the number of CIDR Supplied in the variable
-resource "aws_subnet" "public-subnet" {
-  
+resource "aws_subnet" "public-subnet" { 
   count             = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index)
@@ -14,14 +13,13 @@ resource "aws_subnet" "public-subnet" {
 # Count & Element are complementing functions; Count tells the number of times the loop has to run & element tells the item to picked from that iteration.
 
 # Creates private subnets based on the number of CIDR Supplied in the variable
-resource "aws_subnet" "private-subnet" {
-  
+resource "aws_subnet" "private-subnet" { 
   count             = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.PRIVATE_SUBNET_CIDR, count.index)
   availability_zone = element(var.AZ, count.index)
 
   tags = {
-    Name = "robot-public-subnet-${element(var.AZ, count.index)}"
+    Name = "robot-private-subnet-${element(var.AZ, count.index)}"
   }
 }
