8c5eccfe35e2af297cf3fd77ce76c6ab8be12179
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:17:31 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,10 @@
 # Creates public subnets based on the number of CIDR Supplied in the variable
 resource "aws_subnet" "public-subnet" {
   
-  count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id           = aws_vpc.main.id
+  count             = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
   cidr_block        = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone
+  availability_zone = element(var.AZ, count.index)
 
   tags = {
     Name = "robot-public-subnet-${element(var.AZ, count.index)}"
