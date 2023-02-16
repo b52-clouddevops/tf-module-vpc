b26091b4bc0570cf3832c6f35751a588427fbcee
CodingManoj <mvp2612@hotmail.com>
2023-02-15 21:19:33 -0500

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -13,8 +13,8 @@ resource "aws_subnet" "public-subnet" {
 
 # Count & Element are complementing functions; Count tells the number of times the loop has to run & element tells the item to picked from that iteration.
 
-# Creates pribate subnets based on the number of CIDR Supplied in the variable
-resource "aws_subnet" "public-subnet" {
+# Creates private subnets based on the number of CIDR Supplied in the variable
+resource "aws_subnet" "private-subnet" {
   
   count             = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id            = aws_vpc.main.id
