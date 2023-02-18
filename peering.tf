8ab31711f4bea8de3108f4befc62e6b03f80346b
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:19:44 -0500

    Auto Push Commit

--- peering.tf
+++ peering.tf
@@ -3,10 +3,9 @@
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_CIDR
-  auto_accept   = true
+  auto_accept   = true                  # This options is valid if bpth the VPC's are in the same account
 
   tags = {
     Name = "robot-${var.ENV}-default-vpc-peering"
   }
-
 }
