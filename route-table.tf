48d4826fee724a3002cd1fd58079375c6794a56b
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:22:17 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -26,7 +26,7 @@ resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block                = var.DEFAULTVPC_CIDR
+    cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
 
