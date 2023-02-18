5517e272eefdc55c938b652a1e3bbb5a127ec8f3
CodingManoj <mvp2612@hotmail.com>
2023-02-17 19:48:14 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -12,3 +12,18 @@ resource "aws_route_table" "exapublic-rtmple" {
     Name = "robot-${var.ENV}-public-rt"
   }
 }
+
+# Creating Private Route Table
+
+resource "aws_route_table" "exapublic-rtmple" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "robot-${var.ENV}-public-rt"
+  }
+}
