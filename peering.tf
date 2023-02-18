b73e493f75493ef56e01ea6ae3d8832eed2e3889
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:18:13 -0500

    Auto Push Commit

--- peering.tf
+++ peering.tf
@@ -3,5 +3,5 @@
 resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = 
+  vpc_id        = var.DEFAULT_VPC_CIDR
 }
