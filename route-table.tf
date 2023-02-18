d0e3649910a34d5571d3970a19f5b94f87db16a8
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:00:02 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -22,15 +22,24 @@ resource "aws_route_table_association" "public-rt-association" {
 
 # Creating Private Route Table
 
-# resource "aws_route_table" "private-rt" {
-#   vpc_id = aws_vpc.main.id
+# Creating Public Route Table
 
-#   route {
-#     cidr_block = var.VPC_CIDR
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
 
-#   tags = {
-#     Name = "robot-${var.ENV}-private-rt"
-#   }
-# }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "robot-${var.ENV}-public-rt"
+  }
+}
+
+# Attach the public route table to public-subnets.
+resource "aws_route_table_association" "public-rt-association" {
+  count          = length(aws_subnet.public-subnet.*.id)
+  subnet_id      = element(aws_subnet.public-subnet.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
+}
