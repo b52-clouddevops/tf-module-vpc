39eb8192fa9940fc9afc5dafa0d1932e25f4cf0e
CodingManoj <mvp2612@hotmail.com>
2023-02-17 19:47:31 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -1,7 +1,7 @@
 # Creating Public Route Table
 
 resource "aws_route_table" "exapublic-rtmple" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "10.0.1.0/24"
