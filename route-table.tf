fd1d3c5fc21247a6fdd1ca9cd557bafddd958e10
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:29:04 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -8,6 +8,11 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
+
   tags = {
     Name = "robot-${var.ENV}-public-rt"
   }
