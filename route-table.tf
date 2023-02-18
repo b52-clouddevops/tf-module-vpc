d1fbfb4fe196f13071c2737dfc5114e9bd56fd61
CodingManoj <mvp2612@hotmail.com>
2023-02-17 19:47:39 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -4,8 +4,8 @@ resource "aws_route_table" "exapublic-rtmple" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   route {
