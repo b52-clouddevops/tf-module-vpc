a36173d1283940cdf38592e3869ac9032a0b7baf
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:26:28 -0500

    Auto Push Commit

--- peering.tf
+++ peering.tf
@@ -2,7 +2,7 @@
 
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.DEFAULT_VPC_CIDR
+  vpc_id        = var.DEFAULT_VPC_ID
   auto_accept   = true                  # This options is valid if bpth the VPC's are in the same account
 
   tags = {
