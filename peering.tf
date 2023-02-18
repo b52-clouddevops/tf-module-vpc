1d84be70d9469ec5e8f98c505c39322593547a45
CodingManoj <mvp2612@hotmail.com>
2023-02-17 20:18:29 -0500

    Auto Push Commit

--- peering.tf
+++ peering.tf
@@ -1,7 +1,6 @@
 # Creates peering between robot vpc to Default VPC
 
 resource "aws_vpc_peering_connection" "peering" {
-  peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_CIDR
 }
