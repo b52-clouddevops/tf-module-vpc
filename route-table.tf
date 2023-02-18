13b5ee13d7843334ca94bfcddc5cd54ae1700cab
CodingManoj <mvp2612@hotmail.com>
2023-02-17 19:49:45 -0500

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -19,7 +19,7 @@ resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.
     gateway_id = aws_internet_gateway.igw.id
   }
 
