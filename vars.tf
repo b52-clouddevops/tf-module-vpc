24d5fd0699ef7e6d3c8c936d8da0bdbd8d1d62b0
CodingManoj <mvp2612@hotmail.com>
2023-02-23 19:54:43 -0500

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -2,7 +2,8 @@ variable "VPC_CIDR" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_RT" {}
-variable "PRIVATE_HOSTEDZONE_ID"
+variable "PRIVATE_HOSTEDZONE_ID" {}
+variable "PUBLIC_HOSTEDZONE_ID" {}
 variable "ENV" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
