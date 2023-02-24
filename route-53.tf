9c9f66fa6387d04e2685928c5d0b1454a4cc2779
CodingManoj <mvp2612@hotmail.com>
2023-02-23 19:51:20 -0500

    Auto Push Commit

--- route-53.tf
+++ route-53.tf
@@ -1,2 +1,6 @@
 # By default DNS Translation fo Private Hosted Zone only works with default VPC
-# To make Private DNS Translations to work
+# To make Private DNS Translations to work on a vpc-x, you need to associate the zone with vpc-x
+resource "aws_route53_zone_association" "secondary" {
+  zone_id = aws_route53_zone.example.zone_id
+  vpc_id  = aws_vpc.secondary.id
+}
